c8a2f5f8e5de977d8e65e4d3ee98512b39d16437
Hewalu <henri.priller@gmail.com>
2025-07-08 21:00:19 +0200

    Add GitHub Actions workflow for deploying to GitHub Pages; configure Node.js setup, build process, and deployment steps

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: process.env.NODE_ENV === 'production' ? '/Society-Technology' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Society-Technology/' : '',
 };
 
 export default nextConfig;
